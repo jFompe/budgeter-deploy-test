47916cddd534c066d698b1b2fcf7fc9288bdb675
Jaime Fomperosa <jaime.fompe@gmail.com>
2024-05-18 19:39:10 +0200

    Remove build target

--- front/vite.config.mjs
+++ front/vite.config.mjs
@@ -11,9 +11,6 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/budgeter/",
-  build: {
-    target: 'chrome125'
-  },
   plugins: [
     VueRouter(),
     Vue({
